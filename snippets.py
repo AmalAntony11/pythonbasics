6f49556e468da4b8f2e6985efe5606b12034ca20
AmalAntony11 <69346563+AmalAntony11@users.noreply.github.com>
2020-08-07 16:52:10 +0530

    By Amal Antony
    Code snippets for data structures in python

---
+++ snippets.py
@@ -0,0 +1,18 @@
+#Assigning elements to lists
+amal=[1,2,3,4];
+amal1=[];
+amal1.append(amal[0]);
+amal.append(5);
+f=int(input("enter a number"));
+amal1.append(f);
+print(amal);
+print(amal1);
+#Accessing elements from a tuple
+kali=(1,2,3,4,5);
+print(kali[3]);
+
+#deleting different dictionary elements
+lamal={"Amal":18,"Rahul":20,"Priya":34};
+print(lamal);
+del lamal["Priya"];
+print(lamal);
