5c1a7214d761606ca7641b3a007a85be926c34bf
AmalAntony11 <69346563+AmalAntony11@users.noreply.github.com>
2020-08-07 16:23:30 +0530

    created by Amal Anony
    Simple python program for finding positive numbers within  range

---
+++ positivenumber.py
@@ -0,0 +1,12 @@
+
+amal=[];
+n = int(input("Enter the number of terms"))
+
+for i in range(0,n):
+    amal.append(int(input("Enter the term")));
+print(amal);
+for i in amal:
+    if i>=0:
+        print(i);
+        
+        
